77045eac043e9f3d932e7a14ce921b1a32e0c275
Jônatas Filipe Vieira <jonatas.vieira@contabilizei.com.br>
2022-02-25 00:49:07 -0300

    feat(app.component): Get data from API and show it on page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from "@angular/service-worker";
+import { DataService } from "./data.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pwajokes';
+
+  joke: any;
+
+  update: boolean = false;
+
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
+      // this.update = true;
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
+
+  ngOnInit() {
+    this.data.gimmeJokes().subscribe(res => {
+      this.joke = res;
+    })
+  }
+
 }
